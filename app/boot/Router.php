60a2118e342373dcfba2713c443fbc1a6d511f03
Mario Borna Mjertan <mbm@educateam.hr>
2019-11-07 23:38:23 +0100

    Fixes mentorpanel generation
    Mentors couldn't see their Applications in the mentorpanel because $Session->aai was used instead of $User->aai.
    
    This points to larger issues, such as the need for automated tests and better error handling - accessing an undefined class member shouldn't be possible!

--- app/boot/Router.php
+++ app/boot/Router.php
@@ -89,7 +89,7 @@ switch($route[0]){
         $User->load();
         $Application = new Application($pdo);
         $Applications = $Application->byMentor($Session->user);
-        $Applications2 = $Application->byMentor($Session->aai);
+        $Applications2 = $Application->byMentor($User->aai);
         array_merge($Applications, $Applications2);
         unset($Applications2);
         include '../views/mentorpanel.php';
